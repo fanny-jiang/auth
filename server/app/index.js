4b568ba9cf38f0e510ca37cada9d29fb6a366b6d
Fanny Jiang <jiangf@bu.edu>
2017-04-04 15:40:03 -0400

    sessions working! Cookies!

--- server/app/index.js
+++ server/app/index.js
@@ -2,6 +2,7 @@
 
 var app = require('express')();
 var path = require('path');
+var session = require('express-session');
 
 // "Enhancing" middleware (does not send response, server-side effects only)
 
@@ -11,6 +12,28 @@ app.use(require('./body-parsing.middleware'));
 
 
 // "Responding" middleware (may send a response back to client)
+
+app.use(session({
+  // this mandatory configuration ensures that session IDs are not predictable
+  secret: 'tongiscool', // or whatever you like
+  // these options are recommended and reduce session concurrency issues
+  resave: false,
+  saveUninitialized: false
+}));
+
+// place right after the session setup middleware
+// logs out the session object containing cookie!
+app.use(function (req, res, next) {
+  console.log('session', req.session);
+  next();
+});
+
+app.use('/api', function (req, res, next) {
+  if (!req.session.counter) req.session.counter = 0;
+  console.log('counter', ++req.session.counter);
+  next();
+});
+
 
 app.use('/api', require('../api/api.router'));
 
